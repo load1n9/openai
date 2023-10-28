2bda09843d91db7023aebd068929ef1c9ceb1f2f
Alex Ewerlöf <hanifbbz@gmail.com>
2023-10-28 09:05:37 +0200

    update std to the latest version

--- src/deps.ts
+++ src/deps.ts
@@ -1,2 +1,2 @@
-export { TextDelimiterStream } from "https://deno.land/std@0.189.0/streams/mod.ts";
-export { basename } from "https://deno.land/std@0.189.0/path/mod.ts";
+export { TextDelimiterStream } from "https://deno.land/std@0.204.0/streams/mod.ts";
+export { basename } from "https://deno.land/std@0.204.0/path/mod.ts";
